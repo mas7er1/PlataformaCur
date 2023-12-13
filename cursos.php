0b25b828639d793d5158e9042916692fcc4e4130
Edwin Cortes <edwn81388@gmail.com>
2023-12-13 10:35:11 -0600

    Input V.0

--- cursos.php
+++ cursos.php
@@ -1,6 +1,6 @@
 <?php 
-        include("conexion.php");
-        include("registro_cursos.php");
+    include("conexion.php");
+    include("registro_cursos.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -40,12 +40,60 @@
             <span class="label-char" style="--index: 8">a</span>
             </label> <br>
         </div>
-        <input type="text" placeholder="Nombre del curso" name="nombre_curso">
-        <input type="text" placeholder="Horas del curso" name="horas">
-        <input type="text" placeholder="Division" name="division">
 
-
-
+        <div class="wave-group">
+            <input type="text" name="nombre_curso" class="input">
+            <span class="bar"></span>
+            <label class="label">
+                <span class="label-char" style="--index: 0">N</span>
+                <span class="label-char" style="--index: 1">o</span>
+                <span class="label-char" style="--index: 2">m</span>
+                <span class="label-char" style="--index: 3">b</span>
+                <span class="label-char" style="--index: 4">r</span>
+                <span class="label-char" style="--index: 5">e</span>
+                <span class="label-char" style="--index: 6">d</span>
+                <span class="label-char" style="--index: 7">e</span>
+                <span class="label-char" style="--index: 9">l</span>
+                <span class="label-char" style="--index: 10">C</span>
+                <span class="label-char" style="--index: 11">u</span>
+                <span class="label-char" style="--index: 12">r</span>
+                <span class="label-char" style="--index: 13">s</span>
+                <span class="label-char" style="--index: 14">o</span>
+                </label><br>
+        </div>
+        <div class="wave-group">
+            <input type="text" name="horas" class="input">
+            <span class="bar"></span>
+            <label class="label">
+                <span class="label-char" style="--index: 0">H</span>
+                <span class="label-char" style="--index: 1">o</span>
+                <span class="label-char" style="--index: 2">r</span>
+                <span class="label-char" style="--index: 3">a</span>
+                <span class="label-char" style="--index: 4">s</span>
+                <span class="label-char" style="--index: 5">d</span>
+                <span class="label-char" style="--index: 6">e</span>
+                <span class="label-char" style="--index: 7">l</span>
+                <span class="label-char" style="--index: 8">C</span>
+                <span class="label-char" style="--index: 9">u</span>
+                <span class="label-char" style="--index: 10">r</span>
+                <span class="label-char" style="--index: 11">s</span>
+                <span class="label-char" style="--index: 12">o</span>
+            </label><br>
+        </div>
+        <div class="wave-group">
+            <input type="text" name="division" class="input">
+            <span class="bar"></span>
+            <label class="label">
+                <span class="label-char" style="--index: 0">D</span>
+                <span class="label-char" style="--index: 1">i</span>
+                <span class="label-char" style="--index: 2">v</span>
+                <span class="label-char" style="--index: 3">i</span>
+                <span class="label-char" style="--index: 4">s</span>
+                <span class="label-char" style="--index: 5">i</span>
+                <span class="label-char" style="--index: 6">o</span>
+                <span class="label-char" style="--index: 7">n</span>
+            </label> <br>
+        </div>
         <input type="submit" value="Registrar" name="registro">
     </form>
     <a href="index.php">Salir</a>
