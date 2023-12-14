fec39503423988207ea520c4b0820e103330a87e
Edwin Cortes <edwn81388@gmail.com>
2023-12-14 09:44:24 -0600

    Prueba Rama

--- cursos.php
+++ cursos.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cursos</title>
+    <title>Curso</title>
     <link rel="stylesheet" href="css/cur_styles.css">
 </head>
 <body>
